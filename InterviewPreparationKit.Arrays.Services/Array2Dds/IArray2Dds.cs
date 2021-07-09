f1d60b5a4044913ac64a6e3c7ec94dbdd5aefd83
Yonathan Barrantes <ybarrantes.juntos085@gmail.com>
2021-07-08 19:48:40 -0500

    solve 2D Array - DS exercise

---
+++ InterviewPreparationKit.Arrays.Services/Array2Dds/IArray2Dds.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace InterviewPreparationKit.Arrays.Services.Array2Dds
+{
+    public interface IArray2Dds
+    {
+        int GetHourglassSum(List<List<int>> arr);
+    }
+}
