3fdaf4656bfddee9e601a5609dd9a957b0f172b3
Yonathan Barrantes <ybarrantes.juntos085@gmail.com>
2021-07-22 11:51:28 -0500

    solve mark & toys

---
+++ InterviewPreparationKit.Sorting.Services/MarkAndToys/IMarkAndToys.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace InterviewPreparationKit.Sorting.Services.MarkAndToys
+{
+    public interface IMarkAndToys
+    {
+        int GetMaximumToys(List<int> prices, int k);
+    }
+}
